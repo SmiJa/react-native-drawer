ac2a80db1b2f825032d57fc37935fe79eaec1c57
Jason Smith <jasons@alphaworks.tech>
2022-06-27 17:51:09 -0700

    refactor(drawer): move drawer and screens into seperate components

--- App.js
+++ App.js
@@ -1,35 +1,7 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 import 'react-native-gesture-handler';
-
-function Home() {
-  return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function About() {
-  return (
-    <View>
-      <Text>About Screen</Text>
-    </View>
-  )
-}
-
-const Drawer = createDrawerNavigator();
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator initialRouteName='Home'>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="About" component={About} />
-    </Drawer.Navigator>
-  )
-}
+import MyDrawer from './Navigation/MyDrawer';
 
 export default function App() {
   return (
@@ -38,12 +10,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
